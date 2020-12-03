c5066f7dfdf43af81ffabfc3bd2de19815f663b0
Miroslav Čech <miroslav.cech@gooddata.com>
2020-12-03 15:40:14 +0100

    day_2_2

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::io::{BufRead, BufReader};
 
 #[derive(Debug)]
 struct Policy{
-    lower_bound: usize,
-    upper_bound: usize,
+    position1: usize,
+    position2: usize,
     character: char,
 }
 
@@ -20,19 +20,23 @@ fn main() {
 fn parse_line(line: String) -> (Policy, String){
     let split: Vec<&str> = line.split(|c| c == '-' || c== ' ' || c== ':').collect();
     (Policy{
-        lower_bound: split[0].parse::<usize>().unwrap(),
-        upper_bound: split[1].parse::<usize>().unwrap(),
+        position1: split[0].parse::<usize>().unwrap(),
+        position2: split[1].parse::<usize>().unwrap(),
         character: split[2].parse::<char>().unwrap()
 
     }, split[4].into())
 }
 
 fn complies(line: &(Policy, String)) -> bool{
-   let (Policy{lower_bound, upper_bound, character}, password) = line;
-   let count = password.chars()
-       .filter(|ch| &ch == &character)
-       .count();
-   (lower_bound..&(upper_bound+1)).contains(&&count)
+   let (Policy{position1, position2, character}, password) = line;
+   let letters: Vec<char> = password.chars().collect();
+   let letter1 = letters.get(position1 -1);
+   let letter2 = letters.get(position2 -1);
+   match (letter1, letter2) {
+        (Some(first), Some(second)) if first == character && second != character => true,
+        (Some(first), Some(second)) if first != character && second == character => true,
+        _ => false,
+   }
 }
 
 fn compute< I>(stream: I) -> usize
@@ -44,7 +48,7 @@ fn compute< I>(stream: I) -> usize
 
 #[test]
 fn test_input(){
-    assert_eq!(2,
+    assert_eq!(1,
         compute(vec![
             "1-3 a: abcde".into(),
             "1-3 b: cdefg".into(),
